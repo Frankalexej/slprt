4bf883f2b0103dcb8c2ffad58c7b9d56686e45b3
Frankalexej <460214560@qq.com>
2023-06-04 20:45:17 +0800

    Added readme

--- video2graph.py
+++ video2graph.py
@@ -2,5 +2,4 @@
 # Use Mediapipe to turn video to detected hand landmarks (only hands at the moment)
 # 1. Detect
 # 2. Write into graph file (?)
-# 3. Add it on original video
-
+# 3. Add it on original video, and save the video to another dir in src
